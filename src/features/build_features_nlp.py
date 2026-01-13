a8de3f989f656dafc541f59a689fc7f97a6cc19a
landroni <landroni@users.noreply.github.com>
2026-01-13 05:21:22 +0100

    factor clean-up code in rak_data_cleanup() function

--- src/features/build_features_nlp.py
+++ src/features/build_features_nlp.py
@@ -21,60 +21,67 @@ Rak_test_raw = pd.read_csv('../data/raw/X_test_update.csv', index_col=0)  ##raw 
 
 #####
 ##Clean-up strings
-##FIXME def func
+def rak_data_cleanup(rak_data_raw):
+    ##Fix problematic strings for text clean-up
+    ##FIXME not sure if deep copy inside function is needed
+    rak_data = rak_data_raw.copy(deep = True)
 
-##Fix problematic strings for text clean-up
+    ## Replace NaN with ''
+    ## (for some reason strings can include numeric NaN values)
+    rak_data['description'] = rak_data['description'].fillna('')
 
-Rak_train = Rak_train_raw.copy(deep = True)
+    ## lower case
+    rak_data['designation'] = rak_data['designation'].str.lower()
+    rak_data['description'] = rak_data['description'].str.lower()
 
-## Replace NaN with ''
-## (for some reason strings can include numeric NaN values)
-Rak_train['description'] = Rak_train['description'].fillna('')
+    ##FIXME simplify syntax
+    ##Unescape HTML
+    rak_data['designation'] = rak_data.apply(lambda row: html.unescape(row['designation']), axis = 1)
+    rak_data['description'] = rak_data.apply(lambda row: html.unescape(row['description']), axis = 1)
 
-## lower case
-Rak_train['designation'] = Rak_train['designation'].str.lower()
-Rak_train['description'] = Rak_train['description'].str.lower()
+    ## Remove HTML tags
+    rak_data['designation'] = rak_data.apply(lambda row: BeautifulSoup(row['designation'], "html.parser").get_text(separator=" "), axis = 1)
+    rak_data['description'] = rak_data.apply(lambda row: BeautifulSoup(row['description'], "html.parser").get_text(separator=" "), axis = 1)
 
-##FIXME simplify syntax
-##Unescape HTML
-Rak_train['designation'] = Rak_train.apply(lambda row: html.unescape(row['designation']), axis = 1)
-Rak_train['description'] = Rak_train.apply(lambda row: html.unescape(row['description']), axis = 1)
-
-## Remove HTML tags
-Rak_train['designation'] = Rak_train.apply(lambda row: BeautifulSoup(row['designation'], "html.parser").get_text(separator=" "), axis = 1)
-Rak_train['description'] = Rak_train.apply(lambda row: BeautifulSoup(row['description'], "html.parser").get_text(separator=" "), axis = 1)
-
-##FIXME drop empty designation & description rows
-##FIXME make fun to clean-up data (to apply to test as well)
-
-
-##FIXME list of problematic strings to fix
-## àªtre
-
-##Regex replacements
-repl_dict = {
-             ##FIXME nltk.classify.textcat.TextCat().remove_punctuation() 
-             r'n°': r' numéro ', 
-
-             ##FIXME not sure how to handle '¿' or '?'
-             ##insert space around any non-digit, non-word and non-whitespace with (e.g. '\?' -> ' \? ', 'n°' -> 'n ° ')
-             ##except ¿'
-             r"[^\d\w\s¿\?'\-]": r' \g<0> ',  
-
-             ##FIXME possibly remove digits after translation
-             r"\b\S*[0-9]+\S*\b": '' ##drop all words that contain digits (so drop all digits as well)
-            }
-
-Rak_train = Rak_train.replace(to_replace = {'designation': repl_dict,
-                                               'description': repl_dict},
-                                 regex = True)
+    ##FIXME drop empty designation & description rows
+    ##FIXME make fun to clean-up data (to apply to test as well)
 
 
+    ##FIXME list of problematic strings to fix
+    ## àªtre
 
-##Concat strings
-Rak_train['product_txt'] = Rak_train['designation'] + ' . -//- ' + Rak_train['description']
+    ##Regex replacements
+    repl_dict = {
+                ##FIXME nltk.classify.textcat.TextCat().remove_punctuation() 
+                r'n°': r' numéro ', 
 
-Rak_train['product_txt_len'] = Rak_train['product_txt'].apply(len)
+                ##FIXME not sure how to handle '¿' or '?'
+                ##insert space around any non-digit, non-word and non-whitespace with (e.g. '\?' -> ' \? ', 'n°' -> 'n ° ')
+                ##except ¿'
+                r"[^\d\w\s¿\?'\-]": r' \g<0> ',  
+
+                ##FIXME possibly remove digits after translation
+                r"\b\S*[0-9]+\S*\b": '' ##drop all words that contain digits (so drop all digits as well)
+                }
+
+    rak_data = rak_data.replace(to_replace = {'designation': repl_dict,
+                                                'description': repl_dict},
+                                    regex = True)
+
+
+
+    ##Concat strings
+    rak_data['product_txt'] = rak_data['designation'] + ' . -//- ' + rak_data['description']
+
+    rak_data['product_txt_len'] = rak_data['product_txt'].apply(len)
+
+    return rak_data
+
+##clean-up X train data
+Rak_train = rak_data_cleanup(Rak_train_raw)
+
+##clean-up X test data
+Rak_test = rak_data_cleanup(Rak_test_raw)
 
 
 #####
